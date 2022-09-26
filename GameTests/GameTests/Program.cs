27ab57fead7609028b0d27222143d64e70a0de38
ESCG\530240 <530240@chrome.escg.ac.uk>
2022-09-26 16:41:12 +0100

    Create/Upload Name

---
+++ GameTests/GameTests/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string name;
+        Console.WriteLine("Enter your name to start: ");
+        name = Console.ReadLine();
+        Console.Clear();
+        Thread.Sleep(1000);
+        Console.WriteLine("Welcome, " + name + " to my game.");
+    }
+}
