6e6f6d9d6eeeae15c87d813e99a8044f3c79c799
ESCG\530240 <530240@chrome.escg.ac.uk>
2022-09-27 17:00:05 +0100

    Started tutorial + startscreen

--- GameTests/GameTests/Program.cs
+++ GameTests/GameTests/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 public class Program
 {
@@ -14,11 +15,11 @@ public class Program
 
         while (correct == false)
         {
-            Console.WriteLine("Enter your name to start: ");
+            Console.Write("Enter your name to start: ");
             name = Console.ReadLine();
             Console.Clear();
             Thread.Sleep(200);
-            Console.WriteLine($"{name}? Is that correct?");
+            Console.Write($"{name}? Is that correct? ");
             answer = Console.ReadLine().ToLower();
             correct = answer.Contains("y");
             Console.Clear();
@@ -26,7 +27,9 @@ public class Program
 
         Console.WriteLine($"Well then, {name}, welcome to my game.");
         Thread.Sleep(1000);
-        Console.WriteLine("It's a pleasure to have you here, do you know how to play this game?");
+        Console.Write("It's a pleasure to have you here, ");
+        Thread.Sleep(1000);
+        Console.Write("do you know how to play this game? ");
         answer = Console.ReadLine().ToLower();
         correct = answer.Contains("y");
         Console.Clear();
@@ -37,12 +40,37 @@ public class Program
             Console.WriteLine("Wow, that makes this easy.");
             Thread.Sleep(2000);
             Console.WriteLine("I'll just plop you right into gameplay then, enjoy!");
-            Thread.Sleep(5000);
+            Thread.Sleep(4000);
             Console.Clear();
+            StartScreen();
         }
         else if (correct == false)
         {
             Console.WriteLine("Well then, I'll take you through the tutorial.");
+            Thread.Sleep(2000);
+            Tutorial();
+        }
+
+
+        void Tutorial()
+        {
+            Console.Clear();
+            Console.Write("So, you're new to the game, hm? ");
+            Thread.Sleep(2000);
+            Console.WriteLine("Don't worry though, I'll help you get on your feet:) !!");
+            Thread.Sleep(2000);
+            Console.WriteLine("The main idea of this game is to explore multiple different places such as cities, mountains etc..");
+            Thread.Sleep(3000);
+            Console.WriteLine("Along the way, you can tame and collect cats.");
+            Thread.Sleep(2000);
+            Console.WriteLine("However, there are some issues you come across, which will be revealed to you later on in the game.");
+            Thread.Sleep(4000);
+        }
+        
+
+        void StartScreen()
+        {
+            Console.WriteLine("Welcome!!!!!!!!!!!!");
         }
     }
 }
