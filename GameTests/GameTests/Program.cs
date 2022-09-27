5d93c748cb6e897096b897cedfcc9bae948d0049
ESCG\530240 <530240@chrome.escg.ac.uk>
2022-09-27 16:25:07 +0100

    Ask name + ask for tutorial

--- GameTests/GameTests/Program.cs
+++ GameTests/GameTests/Program.cs
@@ -4,11 +4,45 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        string name;
-        Console.WriteLine("Enter your name to start: ");
-        name = Console.ReadLine();
-        Console.Clear();
+        string name = "name";
+        bool correct = false;
+        string answer = "answer";
+        int playerhealth = 100;
+        int cathealth = 100;
+
+
+
+        while (correct == false)
+        {
+            Console.WriteLine("Enter your name to start: ");
+            name = Console.ReadLine();
+            Console.Clear();
+            Thread.Sleep(200);
+            Console.WriteLine($"{name}? Is that correct?");
+            answer = Console.ReadLine().ToLower();
+            correct = answer.Contains("y");
+            Console.Clear();
+        }
+
+        Console.WriteLine($"Well then, {name}, welcome to my game.");
         Thread.Sleep(1000);
-        Console.WriteLine("Welcome, " + name + " to my game.");
+        Console.WriteLine("It's a pleasure to have you here, do you know how to play this game?");
+        answer = Console.ReadLine().ToLower();
+        correct = answer.Contains("y");
+        Console.Clear();
+
+
+        if (correct == true)
+        {
+            Console.WriteLine("Wow, that makes this easy.");
+            Thread.Sleep(2000);
+            Console.WriteLine("I'll just plop you right into gameplay then, enjoy!");
+            Thread.Sleep(5000);
+            Console.Clear();
+        }
+        else if (correct == false)
+        {
+            Console.WriteLine("Well then, I'll take you through the tutorial.");
+        }
     }
 }
